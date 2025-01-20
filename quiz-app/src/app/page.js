446a0da62204fca847bd4b24351423382fad59fb
Adnan <101324354+Adnanoff029@users.noreply.github.com>
2025-01-20 05:30:33 +0530

    quiz page

--- quiz-app/src/app/page.js
+++ quiz-app/src/app/page.js
@@ -1,5 +1,5 @@
 import getQuestions from "@/hooks/getQuestions";
 
 export default async function Home() {
-  return <div className="">{JSON.stringify(questions)}</div>;
+  return <div className=""></div>;
 }
--- quiz-app/src/components/Questions.js
+++ quiz-app/src/components/Questions.js
@@ -1,19 +1,47 @@
 "use client";
 import getQuestions from "@/hooks/getQuestions";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Questions() {
   const [questions, setQuestions] = useState(null);
   const [questionNumber, setQuestionNumber] = useState(0);
+  const [answers, setAnswers] = useState([]);
+  const question = useRef(null);
+  const options = useRef(null);
+  const handleClick = (e, idx) => {
+    setQuestionNumber(() => idx);
+  };
 
-  const handleClick = () => {};
+  const handleOptionClick = (e, idx) => {
+    //   console.log(e.target.innerText.split(' ').join(' '));
+    setAnswers((prev) => {
+      prev[questionNumber] = e.target.innerText;
+      return [...prev];
+    });
+    console.log(answers);
+  };
+  useEffect(() => {
+    if (!questions) return;
+    question.current.innerHTML =
+      questions && questions[questionNumber].question;
+    // const elements = questions[questionNumber].options.map(
+    //   (ele) =>
+    //     `<div
+    //       className="p-3 text-[20px] text-black font-[500] transition ease-in-out rounded-md cursor-pointer hover:bg-blue-500 hover:text-white"
+    //       key={${ele.id}}
+    //     >
+    //       ${ele.option}
+    //     </div>`
+    // );
+    // elements.forEach((ele) => {
+    //     console.log(ele);
+    //   options.current.appendChild(document.createElement(ele));
+    // });
+  }, [questionNumber, questions]);
 
   useEffect(() => {
-    const div = document.getElementById("question");
-    if (questions) {
-      div.innerHTML = questions[questionNumber].question;
-    }
-  }, [questions, questionNumber]);
+    if (!options) return;
+  }, [options]);
 
   useEffect(() => {
     const getData = async () => {
@@ -27,22 +55,36 @@ export default function Questions() {
           incorrect_answers,
           correct_answer,
           isDone: false,
-          options: [...incorrect_answers, correct_answer],
+          visited: idx === 0 ? true : false,
+          options: [...incorrect_answers, correct_answer].map((ele, idx) => {
+            return {
+              id: idx,
+              option: ele,
+              isSelected: false,
+            };
+          }),
+          optionSelected: -1,
         };
       });
       setQuestions(() => finalData);
     };
     getData();
+    setAnswers(() => new Array(15).fill(""));
   }, []);
+
   return (
     <div className="grid grid-cols-3 gap-4 h-[100vh]">
       <div className="bg-blue-300 flex flex-col items-center py-10">
         <h1 className="text-3xl font-[600]">Index</h1>
         <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2items-center md:gap-4 gap-2 my-8">
           {questions ? (
-            questions.map((_, idx) => (
-              <div className="p-4 bg-gray-300 text-center rounded-[100px] cursor-pointer" key={idx} onClick={handleClick}>
-                {idx + 1}
+            questions.map((ele) => (
+              <div
+                className="p-3 bg-purple-600 text-white text-center font-[500] transition ease-in-out rounded-md cursor-pointer"
+                key={ele.id}
+                onClick={(e) => handleClick(e, ele.id)}
+              >
+                {ele.id + 1}
               </div>
             ))
           ) : (
@@ -50,15 +92,30 @@ export default function Questions() {
           )}
         </div>
       </div>
-      <div className="col-span-2 flex flex-col items-center justify-center p-2">
-        {questions ? (
-          <div className="text-2md">
-            <div id="question"></div>
-            <div>{questions[questionNumber].correct_answer}</div>
-          </div>
-        ) : (
-          <div>Oops No questions to display</div>
-        )}
+      <div className="col-span-2 flex flex-col items-start p-2 w-[100%] my-[10%]">
+        <div className="text-2xl font-[500]" ref={question}></div>
+        <div className="text-md font-[400] mt-3 w-[100%]" ref={options}>
+          {questions &&
+            questions[questionNumber].options.map((ele) => {
+              return (
+                <div
+                  className="p-2 text-[20px] text-black font-[500] transition ease-in-out rounded-md cursor-pointer hover:bg-blue-500 hover:text-white w-[100%]"
+                  style={
+                    answers[questionNumber] === ele.option
+                      ? {
+                          backgroundColor: "rgba(24, 125, 245, 0.8)",
+                          color: "white",
+                        }
+                      : {}
+                  }
+                  key={ele.id}
+                  onClick={(e) => handleOptionClick(e, ele.id)}
+                >
+                  {ele.option}
+                </div>
+              );
+            })}
+        </div>
       </div>
     </div>
   );
